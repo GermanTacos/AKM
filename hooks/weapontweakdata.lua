f997df0fcba3f330910cc23136c3288e60686921
GermanTacos <jakebuilds@gmail.com>
2019-10-31 01:49:24 -0400

    Fixed the damn crash + CAP for muzzle devices and gadgets
    there was no dummy barrel whoops

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -5,7 +5,14 @@ if ( self.ak_stamp_762 ) then
 	self:SetupAttachmentPoint("ak_stamp_762", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, 5.3, 0 ), 
+        position = Vector3( 0, 1.9, -0.1 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	
+	self:SetupAttachmentPoint("ak_stamp_762", {
+		name = "a_fl", 
+        base_a_obj = "a_fl", 
+        position = Vector3( -0.75, -2, -1.5 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	
