f08de29da33832e1628ddd2406ccea67493c65ff
GermanTacos <jakebuilds@gmail.com>
2019-10-31 00:24:11 -0400

    Attempted (poorly) to fix crash

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,35 +1,8 @@
-Hooks:PostHook( WeaponFactoryTweakData, "init", "STG58modInit", function(self)
+Hooks:PostHook( WeaponFactoryTweakData, "init", "AKMInit", function(self)
 	
-	self.parts.wpn_fps_ass_fusil_762_fh_stg58.pcs = nil
-	
-	self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762 = {translation = Vector3(0, 5, -3.05)}
-	self.parts.wpn_fps_upg_o_aimpoint.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_docter.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_eotech.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_t1micro.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_aimpoint_2.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_eotech_xps.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_reflex.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_rx01.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_rx30.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_cmore.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_cs.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_fusil_762)
-	self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_ass_fusil_762 = {translation = Vector3(0, 2, -3.05)}
-	self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_scar)
-	self.parts.wpn_fps_upg_o_spot.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_spot.stance_mod.wpn_fps_ass_scar)
-	self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_ass_scar)
-	self.parts.wpn_fps_upg_o_45rds_v2.stance_mod.wpn_fps_ass_fusil_762 = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_fusil_762)
-	
-	
-	for _, part_id in pairs(self.wpn_fps_ass_fusil_762.uses_parts) do
-		if self.parts[part_id].type == "sight" then
-			self.wpn_fps_ass_fusil_762.adds[part_id] = {"wpn_fps_ass_fusil_762_sm_modern"}
-			if part_id ~= "wpn_fps_upg_o_acog" then
-				self.parts.wpn_fps_ass_fusil_762_dc_sa58.override[part_id] = 	{a_obj="a_o_sa58", stance_mod = {wpn_fps_ass_fusil_762 = {translation = Vector3(0, 5, -2.85)}}}
-			else
-				self.parts.wpn_fps_ass_fusil_762_dc_sa58.override[part_id] = 	{a_obj="a_o_sa58", stance_mod = {wpn_fps_ass_fusil_762 = {translation = Vector3(0, 2, -2.85)}}}
-			end
-		end
-	end
+--=========================--
+--            AKM          --
+-- GERMANTACOS, KILLERWOLF --
+--=========================--
 	
 end )
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,44 +1,14 @@
-Hooks:PostHook( WeaponTweakData, "init", "STG58modInit", function(self)
+Hooks:PostHook( WeaponTweakData, "init", "AKMInit", function(self)
 
-if ( self.fusil_762 ) then
---- Base stuff ---	
-	self:SetupAttachmentPoint("fusil_762", {
+if ( self.ak_stamp_762 ) then
+	
+	self:SetupAttachmentPoint("ak_stamp_762", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, 5.15, -0.25 ), -- -16
+        position = Vector3( 0, 5.3, 0 ), 
         rotation = Rotation( 0, 0, 0 ) 
-	})
-	self:SetupAttachmentPoint("fusil_762", {
-		name = "a_fl", 
-        base_a_obj = "a_fl", 
-        position = Vector3( -2, 14, -2.8 ), 
-        rotation = Rotation( 0, 0, 90 ) 
-	})
-	self:SetupAttachmentPoint("fusil_762", {
-		name = "a_gl", 
-        base_a_obj = "a_fl", 
-        position = Vector3( -2.88, 27.2, -3.1 ), 
-        rotation = Rotation( 0, 0, 0 ) 
-	})
-	self:SetupAttachmentPoint("fusil_762", {
-		name = "a_o", 
-        base_a_obj = "a_o", 
-        position = Vector3( 0, -9, -1.3 ), 
-        rotation = Rotation( 0, 0, 0 ) 
-	})
-	self:SetupAttachmentPoint("fusil_762", {
-		name = "a_os", 
-        base_a_obj = "a_os", 
-        position = Vector3( 0, 1.5, 1.1 ), 
-        rotation = Rotation( 0, 10, 0 )
 	})
 	
-	self:SetupAttachmentPoint("fusil_762", {
-		name = "a_o_sa58", 
-        base_a_obj = "a_o", 
-        position = Vector3( 0, -9, -1.5 ), 
-        rotation = Rotation( 0, 0, 0 ) 
-	})
 end
 
-end )
+end)
