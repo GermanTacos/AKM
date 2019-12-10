21a5492deeff58947fe90f52f8d808fedb8b41c4
GermanTacos <jakebuilds@gmail.com>
2019-12-10 04:00:20 -0500

    Hmm, today I will

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,8 +1,24 @@
-Hooks:PostHook( WeaponFactoryTweakData, "init", "AKMInit", function(self)
-	
 --=========================--
 --            AKM          --
 -- GERMANTACOS, KILLERWOLF --
 --=========================--
-	
+
+--=========================--
+--        FUNCTIONS        --
+--=========================--
+
+--Pretend there are functions here
+
+Hooks:PostHook( WeaponFactoryTweakData, "init", "AKMInit", function(self)
+
+--=========================--
+--          ARRAYS         --
+--=========================--
+
+--Pretend there are arrays here
+
+--=========================--
+--      COMPATIBILITY      --
+--=========================--
+
 end )
